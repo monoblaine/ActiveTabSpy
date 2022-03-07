afbf56848ce65494369fd9776408d829423659fb
Serhan Apaydın <zerhan@gmail.com>
2022-03-08 02:56:02 +0300

    Add support for VS2022 horizontal tabs

---
+++ ActiveTabSpy/Vs2022.cpp
@@ -0,0 +1,51 @@
+#include "pch.h"
+#include "common.h"
+
+const COLORREF activeTabColor = RGB(0x00, 0x6C, 0xBE);
+
+class Vs2022 : public Inspectable {
+    public:
+    virtual IUIAutomationElement* findActiveTab(IUIAutomationElement* windowEl, bool isHorizontal) {
+        IUIAutomationElement* el = windowEl;
+        getFirstChildElement(&el, false);
+
+        if (!el) {
+            return nullptr;
+        }
+
+        do getNextSiblingElement(&el);
+        while (el && !isClsMatch(el, L"DockRoot"));
+
+        if (!el) {
+            return nullptr;
+        }
+
+        if (isHorizontal) {
+            getFirstChildElement(&el); // Document group
+            IUIAutomationSelectionPattern* selectionPattern = nullptr;
+            IUIAutomationElementArray* selections = nullptr;
+            IUIAutomationElement* selection = nullptr;
+            el->GetCurrentPatternAs(
+                UIA_SelectionPatternId,
+                __uuidof(IUIAutomationSelectionPattern),
+                (void**) &selectionPattern
+            );
+            el->Release();
+            selectionPattern->GetCurrentSelection(&selections);
+            selectionPattern->Release();
+            selections->GetElement(0, &selection);
+            selections->Release();
+
+            return selection;
+        }
+        else {
+            return nullptr; // not tested.
+        }
+    }
+};
+
+Vs2022 inspectable;
+
+extern "C" __declspec(dllexport) void inspectActiveTabOnVs2022(HWND hWnd, int isHorizontal, int* pointX, int* pointY, int* left, int* right, int* top, int* bottom) {
+    inspectActiveTab(hWnd, isHorizontal, pointX, pointY, left, right, top, bottom, &inspectable);
+}
