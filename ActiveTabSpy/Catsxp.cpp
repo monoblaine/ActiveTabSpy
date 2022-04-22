a334b5b7b8321dba88a385cc289ccbcfd385198f
Serhan Apaydın <zerhan@gmail.com>
2022-04-22 12:35:49 +0300

    Add Catsxp support

---
+++ ActiveTabSpy/Catsxp.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "common.h"
+
+extern "C" __declspec(dllexport) void getCatsxpThreeDotBtnStatus(HWND hWnd, int* result) {
+    IUIAutomationElement* el = nullptr;
+    getWindowEl(hWnd, &el);
+    getLastChildElement(&el);   // Catsxp
+    getFirstChildElement(&el);
+    getLastChildElement(&el);
+    getFirstChildElement(&el);
+    getFirstChildElement(&el);
+    getNextSiblingElement(&el); // toolbar
+    getLastChildElement(&el);
+
+    BOOL isFocused;
+    el->get_CurrentHasKeyboardFocus(&isFocused);
+    el->Release();
+    *result = isFocused ? 1 : 0;
+}
