148d2dc53482c9622150872e5aa720d2ca7c57f0
Serhan Apaydın <zerhan@gmail.com>
2022-05-08 23:29:06 +0300

    Export method "isCatsxpAddressBarFocused" for Catsxp

--- ActiveTabSpy/Catsxp.cpp
+++ ActiveTabSpy/Catsxp.cpp
@@ -17,3 +17,24 @@ extern "C" __declspec(dllexport) void getCatsxpThreeDotBtnStatus(HWND hWnd, int*
     el->Release();
     *result = isFocused ? 1 : 0;
 }
+
+extern "C" __declspec(dllexport) void isCatsxpAddressBarFocused(HWND hWnd, int* result) {
+    IUIAutomationElement* el = nullptr;
+    getWindowEl(hWnd, &el);
+    getLastChildElement(&el);   // Catsxp
+    getFirstChildElement(&el);
+    getLastChildElement(&el);
+    getFirstChildElement(&el);
+    getFirstChildElement(&el);
+    getNextSiblingElement(&el); // toolbar
+    getFirstChildElement(&el);
+
+    while (!isOfType(el, UIA_EditControlTypeId)) {
+        getNextSiblingElement(&el);
+    }
+
+    BOOL isFocused;
+    el->get_CurrentHasKeyboardFocus(&isFocused);
+    el->Release();
+    *result = isFocused ? 1 : 0;
+}
