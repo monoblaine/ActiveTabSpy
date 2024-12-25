3394880b519fec863b127aeff87380f48db81969
Serhan Apaydın <zerhan@gmail.com>
2024-12-25 20:28:46 +0300

    lint

--- ActiveTabSpy/MsEdge.cpp
+++ ActiveTabSpy/MsEdge.cpp
@@ -5,25 +5,20 @@ extern "C" __declspec(dllexport) int MsEdge_getThreeDotBtnStatus(HWND hWnd) {
     IUIAutomationElement* el = nullptr;
     getWindowEl(hWnd, &el);
     getFirstChildElement(&el); // Intermediate D3D Window
-
     while (el) {
         if (getClassName(el) == L"BrowserRootView") {
             break;
         }
-
         getNextSiblingElement(&el);
     }
-
     auto hr = findFirstElementByClassName(&el, L"BrowserAppMenuButton");
     BOOL isFocused;
-
     if (SUCCEEDED(hr)) {
         el->get_CurrentHasKeyboardFocus(&isFocused);
     }
     else {
         isFocused = false;
     }
-
     el->Release();
     return isFocused ? 1 : 0;
 }
